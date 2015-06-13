a45909515cd80f47dab08b6572e98cc083fe2996
Steve Perkins <steve@steveperkins.com>
2015-06-13 10:10:29 -0400

    Initial commit.  'Blog' project is at the end of Week 2, while the other exercises are at the end of Week 3.

---
+++ Homework2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using MongoDB.Bson;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main()
+        {
+            MainAsync().Wait();
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync()
+        {
+            var client = new MongoClient();
+            var db = client.GetDatabase("students");
+            var collection = db.GetCollection<BsonDocument>("grades");
+
+            // Iterate through all students one at a time
+            var documents = await collection.Find(new BsonDocument()).ToListAsync();
+            foreach (var studentId in documents.GroupBy(doc => doc["student_id"]))
+            {
+                // Iterate through all of the "homework" scores for this student
+                var filterBuilder = Builders<BsonDocument>.Filter;
+                var homeworkFilter = filterBuilder.Eq("student_id", studentId.Key) & filterBuilder.Eq("type", "homework");
+                var homeworkScores = await collection.Find(homeworkFilter).ToListAsync();
+
+                // Identify the lowest score
+                BsonDocument lowestScore = null;
+                foreach (var score in homeworkScores)
+                {
+                    if (lowestScore == null || score["score"] < lowestScore["score"])
+                    {
+                        lowestScore = score;
+                    }
+                }
+
+                // Remove that score from the collection
+                if (lowestScore != null)
+                {
+                    await collection.DeleteOneAsync(doc => doc["_id"] == lowestScore["_id"]);
+                }
+            }
+        }
+    }
+}
---
+++ Homework3/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+
+namespace Homework3
+{
+    class Student
+    {
+        public double _id { get; set; }
+        public string name { get; set; }
+        public List<Score> scores { get; set; }
+        public BsonDocument ExtraElements { get; set; }
+    }
+    class Score
+    {
+        public string type { get; set; }
+        public double score { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MainAsync().Wait();
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync()
+        {
+            BsonClassMap.RegisterClassMap<Student>();
+            var client = new MongoClient();
+            var db = client.GetDatabase("school");
+            var collection = db.GetCollection<Student>("students");
+
+            // Iterate through all students one at a time
+            var students = await collection.Find(new BsonDocument()).ToListAsync();
+            foreach (var student in students)
+            {
+                // Identify the lowest homework score for this student
+                double lowestScore = -1.0;
+                foreach (var score in student.scores)
+                {
+                    if (score.type == "homework")
+                    {
+                        if (lowestScore == -1 || score.score < lowestScore) 
+                            lowestScore = score.score;
+                    }
+                }
+
+                // Remove that score
+                foreach (var score in student.scores)
+                {
+                    if (score.type == "homework" && score.score == lowestScore)
+                    {
+                        student.scores.Remove(score);
+                        Console.WriteLine("Removing score {0} for student {1}", score.score, student.name);
+                        break;
+                    }
+                }
+
+                // Update the student in the DB
+                var result = await collection.ReplaceOneAsync(
+                    Builders<Student>.Filter.Eq("_id", student._id),
+                    student
+                );
+            }
+        }
+    }
+}
